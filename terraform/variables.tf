01b62e6cf9b8276056d2470ccf2e2a7920796143
HoruzGiltech <giltechnology.2025@gmail.com>
2026-09-16 15:08:12 -0400

    se actualiza archivo variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,12 @@
-variable "db_password" {
-  description = "Contraseña para el usuario administrador de la base de datos"
+variable "db_name" {
+  description = "Nombre de la base de datos PostgreSQL"
   type        = string
-  sensitive   = true # 🛡️ Oculta el valor en la consola por seguridad
+  default     = "login_db"
 }
 
-variable "db_name" {
-  description = "Nombre de la base de datos para la app de login"
+variable "db_password" {
+  description = "Contraseña para el usuario de la base de datos"
   type        = string
-  default     = "db_login_app"
+  sensitive   = true
+  default     = "SuperPasswordSeguro123!" # 💡 Valor por defecto para pruebas de laboratorio
 }
